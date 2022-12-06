b6c0d6eacdbeb27122d6f36f09a7b66800069a3a
mpsinghk <mpsinghk@users.noreply.github.com>
2022-12-05 21:50:35 -0800

    Added compute function

--- assets/calculator.js
+++ assets/calculator.js
@@ -7,52 +7,131 @@ class Calculator extends HTMLElement {
     this.innerHTML = `
       <div class="calculator">
         <div class="screen">
-          <div class="equation">
-            12345 + 6789 - 1234567 &divide; 8912 * 56
-          </div>
-          <div class="result">= 34872934829</div>
+          <div class="equation"></div>
+          <div class="result"></div>
         </div>
 
-        <div class="key operator" data-operator="AC">AC</div>
-        <div class="key operator" data-operator="&plusmn;">&plusmn;</div>
-        <div class="key operator" data-operator="&percnt;">&percnt;</div>
-        <div class="key operator" data-operator="&divide;">&divide;</div>
+        <div class="key highlight" data-clear>AC</div>
+        <div class="key highlight" data-inverse>±</div>
+        <div class="key highlight" data-operator>%</div>
+        <div class="key highlight" data-operator>/</div>
 
-        <div class="key" data-number="7">7</div>
-        <div class="key" data-number="8">8</div>
-        <div class="key" data-number="9">9</div>
-        <div class="key operator" data-operator="&times;">&times;</div>
+        <div class="key" data-operand>7</div>
+        <div class="key" data-operand>8</div>
+        <div class="key" data-operand>9</div>
+        <div class="key highlight" data-operator>*</div>
 
-        <div class="key" data-number="4">4</div>
-        <div class="key" data-number="5">5</div>
-        <div class="key" data-number="6">6</div>
-        <div class="key operator" data-operator="&minus;">&minus;</div>
+        <div class="key" data-operand>4</div>
+        <div class="key" data-operand>5</div>
+        <div class="key" data-operand>6</div>
+        <div class="key highlight" data-operator>-</div>
 
-        <div class="key" data-number="1">1</div>
-        <div class="key" data-number="2">2</div>
-        <div class="key" data-number="3">3</div>
-        <div class="key operator" data-operator="&plus;">&plus;</div>
+        <div class="key" data-operand>1</div>
+        <div class="key" data-operand>2</div>
+        <div class="key" data-operand>3</div>
+        <div class="key highlight" data-operator>+</div>
 
-        <div class="key" data-number=".">.</div>
-        <div class="key" data-number="0">0</div>
-        <div class="key" data-operator="&#9003;">&#9003;</div>
-        <div class="key operator" data-operator="&equals;">&equals;</div>
+        <div class="key" data-operand>.</div>
+        <div class="key" data-operand>0</div>
+        <div class="key" data-delete>&#9003;</div>
+        <div class="key highlight" data-equals>=</div>
       </div>
     `;
+
+    this.equationElement = this.querySelector('.equation');
+    this.resultElement = this.querySelector('.result');
+    this.allClearElement = this.querySelector('[data-clear]');
+    this.deleteElement = this.querySelector('[data-delete]');
+    this.equalsElement = this.querySelector('[data-equals]');
+    this.operandElements = this.querySelectorAll('[data-operand]');
+    this.operatorElements = this.querySelectorAll('[data-operator]');
+
+    this.allClearElement.addEventListener('click', this.clearOutput.bind(this));
+    this.deleteElement.addEventListener('click', this.removeCharacter.bind(this));
+    this.equalsElement.addEventListener('click', this.compute.bind(this));
+
+    this.operandElements.forEach(element => {
+      element.addEventListener('click', () => {
+        this.appendCharacter(element.textContent.toString());
+      });
+    });
+
+    this.operatorElements.forEach(element => {
+      element.addEventListener('click', () => {
+        this.appendCharacter(element.textContent.toString());
+      })
+    });
+
+    this.equation = '';
+    this.result = '';
   }
 
-  connectedCallback() {
-    this.equation = this.querySelector('.equation');
-    this.result = this.querySelector('.result');
-    this.keys = this.querySelectorAll('[data-number]');
-    this.operators = this.querySelectorAll('[data-operator]');
+  update(eq = this.equationElement.textContent.trim(), res = this.resultElement.textContent.trim()) {
+    if (eq !== this.equationElement.textContent.trim()) {
+      this.equationElement.textContent = eq.trim();
+    }
 
-
-    console.log(this.equation, this.result);
-    console.log(this.keys, this.operators);
+    if (res !== this.resultElement.textContent.trim()) {
+      this.resultElement.textContent = '= ' + res;
+    }
   }
 
+  clearOutput() {
+    this.equation = '';
+    this.result = '';
+    this.update(this.equation, this.result);
+  }
 
+  compute() {
+    this.equation = this.equationElement.textContent.trim();
+    this.result = (new Function('return ' + this.equation)());
+    this.update(this.equation, this.result);
+  }
+
+  removeCharacter() {
+    this.equation = this.equationElement.textContent.trim();
+
+    if (!this.equation.trim()) return;
+    this.equation = this.equation.trim().slice(0, -1);
+
+    this.update(this.equation, this.result.toString());
+  }
+
+  appendCharacter(char) {
+    let opeartor = false;
+
+    // See if the last character is an operator
+    switch (this.equationElement.textContent.trim().slice(-1)) {
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+      case '%':
+        opeartor = true;
+        break;
+    }
+
+    // Return, if decimal character is already icluded
+    if (char === '.' && this.equationElement.textContent.includes(char)) return;
+
+    // Don't begin with below operators
+    if ((char === '+' || char === '*' || char === '/' || char === '%') && !this.equationElement.textContent.trim()) {
+      return;
+    }
+
+    // Dont't add consecutive operators
+    if ((char === '+' || char === '-' || char === '*' || char === '/' || char === '%') && opeartor) {
+      return;
+    }
+
+    if (char === '.' && !this.equationElement.textContent.trim()) {
+      this.equation = '0.';
+    } else {
+      this.equation = this.equationElement.textContent + char.toString();
+    }
+
+    this.update(this.equation, this.result);
+  }
 }
 
 customElements.define('calculator-component', Calculator);
